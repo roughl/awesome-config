ae885ed737bb789560752ce0225e87ee7dc334e9
Raphael <roughl@tuxpad.localdomain>
2010-01-26 17:06:44 +0100

    added icons and terminal app support

--- awesomemenu.lua
+++ awesomemenu.lua
@@ -5,6 +5,8 @@ require "ini"
 
 module ("awesomemenu", package.seeall)
 
+icon_path = "/usr/share/icons/hicolor/16x16/apps/"
+
 function create( path, terminal_cmd  )
 	-- default parameters
 	if path == nil then path = "/usr/share/applications/" end
@@ -18,14 +20,33 @@ function create( path, terminal_cmd  )
 			--ini.ini_print(myini)
 
 			desk_sec = myini["Desktop Entry"]
-			if desk_sec and desk_sec["Exec"] then
+			if desk_sec and desk_sec["Exec"] and desk_sec["Name"] then
 				-- valid .desktop file
-				exec = desk_sec["Exec"]
-				if not desk_sec["Name"] then
-					return 1
+				local exec = desk_sec["Exec"]
+				local j = exec:match("(.*)%%")
+				if j then
+				   exec = j
 				end
-				name = desk_sec["Name"]
-				table.insert(systemmenu, {name, exec})
+				local name = desk_sec["Name"]
+				local icon = desk_sec["Icon"]
+				if desk_sec["Terminal"] == "true" then
+					exec = terminal_cmd .. exec
+				end
+				--print(name,exec,icon)
+				-- is icon given?
+				if icon then
+					-- does icon exist?
+					if lfs.attributes(icon) then
+						table.insert(systemmenu, {name, exec, icon})
+					elseif lfs.attributes(icon_path..icon..".png") then
+						--search icon in default path
+						icon = icon_path..icon..".png"
+						table.insert(systemmenu, {name, exec, icon})
+					end
+					--print(name, exec,icon)
+				else
+					table.insert(systemmenu, {name, exec})
+				end
 			end
 		end
 	end

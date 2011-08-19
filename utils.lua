ca60ebe59aa030970216c7b54c710fd07117b1d7
roughl <r0ugh_l@yahoo.de>
2011-08-19 19:07:55 +0200

    simplify stringsplit()

--- utils.lua
+++ utils.lua
@@ -1,19 +1,11 @@
 
 
 function stringsplit(string, char)
-	-- is there any better way to split a string intu substrings?
-	local oldpos = 0
-	subs = {}
-	while true do
-		local pos = string:find(char, oldpos+1)
-		if pos == nil then
-			table.insert(subs,string:sub(oldpos+1))
-			break
-		end
-		table.insert(subs,string:sub(oldpos,pos-1))
-		oldpos=pos
+	tokens = {}
+	for w in string.gmatch( string, "([^"..char.."]+)" ) do
+		tokens[#tokens+1] = w
 	end
-	return subs
+	return tokens
 end
 
 

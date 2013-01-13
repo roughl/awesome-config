550f0abeff6f62e5eef73896803020aff5ebff55
roughl <r0ugh_l@yahoo.de>
2013-01-13 16:04:38 +0100

    rules: float firefox download window

--- rc.lua
+++ rc.lua
@@ -477,9 +477,13 @@ awful.rules.rules = {
     { rule = { class = "gimp" },
       properties = { floating = true } },
     -- Set Firefox to always map on tag number 1 of screen 1.
-	{ rule = { class = "Firefox" },  properties = {tag = tags[1][1]}},
+	{ rule = { class = "Firefox" },
+	  properties = {tag = tags[1][1]}},
+	{ rule = { instance = "Download" },
+	  properties = { floating = true } },
     -- Set vimprobable to tag 1 of screen 1
-    { rule = { class = "vimprobable" }, properties = { tag = tags[1][1] } },
+    { rule = { class = "vimprobable" },
+	  properties = { tag = tags[1][1] } },
 
 }
 -- }}}
